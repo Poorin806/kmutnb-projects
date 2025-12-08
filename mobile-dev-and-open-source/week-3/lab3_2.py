6ae7d337767f3c2a2170913cfa2c8ab696d01b00
Poorin Thatsakhon <Poorin806@gmail.com>
2025-12-08 23:40:37 +0700

    Update

---
+++ mobile-dev-and-open-source/week-3/lab3_2.py
@@ -0,0 +1,10 @@
+def sum(x, y):
+  return x + y
+
+def minus(x, y):
+  return x - y
+
+
+if __name__ == "__main__":
+  print(sum(1, 2))
+  print(minus(3, 4))
---
+++ mobile-dev-and-open-source/week-3/lab3_3.py
@@ -0,0 +1,5 @@
+import lab3_2 as cal
+from lab3_2 import sum, minus
+
+print(cal.sum(1,5))
+print(sum(1, 5))
